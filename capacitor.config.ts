4354b8b7dce166c22fd6acae1f0c84a6e4e7edac
Starman <miguelca2076@gmail.com>
2024-05-31 04:09:13 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Rift',
+  webDir: 'dist'
+};
+
+export default config;
